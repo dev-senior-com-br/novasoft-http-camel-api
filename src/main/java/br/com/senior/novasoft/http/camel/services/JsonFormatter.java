fbcb2336cc3c6ec3c034e04b2532edb70c4afad0
Lucas Nunes <lucas.nunes@senior.com.br>
2022-05-12 12:48:24 -0300

    Adicionando formatter

--- src/main/java/br/com/senior/novasoft/http/camel/services/JsonFormatter.java
+++ src/main/java/br/com/senior/novasoft/http/camel/services/JsonFormatter.java
@@ -1,9 +1,13 @@
 package br.com.senior.novasoft.http.camel.services;
 
+import br.com.senior.novasoft.http.camel.entities.client.ClienteErrorOutput;
+import br.com.senior.novasoft.http.camel.entities.client.ClienteOutput;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,6 +40,35 @@ public class JsonFormatter {
             exchange.getMessage().setBody(jsonArray.toString());
         } catch (Exception exception) {
             log.error("Error JsonFormatter: {}", exception);
+        }
+    }
+
+    /**
+     * Pega o output de erro
+     *
+     * @param exchange {@link Exchange} - Exchange do camel.
+     */
+    public static void formatterArray(Exchange exchange) {
+        try {
+            List<ClienteErrorOutput> clienteErrorOutputs = new ArrayList<>();
+
+            List<Map<String, Object>> mapArray = (List<Map<String, Object>>) exchange.getMessage().getBody();
+            mapArray.forEach(map -> {
+                JsonObject jsonObject = new JsonObject(map);
+                log.info("Json Formatter Array: {}", jsonObject.toString());
+
+                clienteErrorOutputs.add(
+                    new ClienteErrorOutput(
+                        jsonObject.getString("cliente"),
+                        jsonObject.getString("mensaje"),
+                        jsonObject.getBoolean("indError")
+                    )
+                );
+            });
+
+            exchange.getMessage().setBody(new ClienteOutput(clienteErrorOutputs));
+        } catch (Exception exception) {
+            log.error("Error JsonFormatter Array: {}", exception);
         }
     }
 }
