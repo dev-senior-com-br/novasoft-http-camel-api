63b6a6250afd72a93601db8edd6d1e577432bbf2
Lucas Nunes <lucas.nunes@senior.com.br>
2022-06-15 09:00:44 -0300

    Corrigindo nomes Enum

--- src/main/java/br/com/senior/novasoft/http/camel/utils/enums/FromToFieldsAccountEnum.java
+++ src/main/java/br/com/senior/novasoft/http/camel/utils/enums/FromToFieldsAccountEnum.java
@@ -4,22 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum FromToFieldsAccountEnum {
-    codCli("Codigo de cliente", "CodCli"),
-    nomCli("Nombre / Razón Social", "NomCli"),
-    nom1Cli("Nombre / Razón Social", "Nom1Cli"),
-    nitCli("NIT", "NitCli"),
-    codCiu("Ciudad", "CodCiu"),
-    codDep("Departamento", "CodDep"),
-    codPai("País", "CodPai"),
-    di1Cli("Dirección", "Di1Cli"),
-    di2Cli("Distrito", "Di2Cli"),
-    te1Cli("Teléfono", "Te1Cli"),
-    tipCli("Tipo de cuenta", "TipCli"),
-    eMail("Correo electrónico general", "EMail"),
-    ap1Cli("Nombre Corto", "Ap1Cli"),
-    tipPer("Tipo de persona", "TipPer"),
-    estCli("Activo", "EstCli"),
-    pagWeb("Sitio web", "PagWeb");
+    COD_CLI("Codigo de cliente", "CodCli"),
+    NOM_CLI("Nombre / Razón Social", "NomCli"),
+    NOM1_CLI("Nombre / Razón Social", "Nom1Cli"),
+    NIT_CLI("NIT", "NitCli"),
+    COD_CIU("Ciudad", "CodCiu"),
+    COD_DEP("Departamento", "CodDep"),
+    COD_PAI("País", "CodPai"),
+    DI1_CLI("Dirección", "Di1Cli"),
+    DI2_CLI("Distrito", "Di2Cli"),
+    TE1_CLI("Teléfono", "Te1Cli"),
+    TIP_CLI("Tipo de cuenta", "TipCli"),
+    E_MAIL("Correo electrónico general", "EMail"),
+    AP1_CLI("Nombre Corto", "Ap1Cli"),
+    TIP_PER("Tipo de persona", "TipPer"),
+    EST_CLI("Activo", "EstCli"),
+    PAG_WEB("Sitio web", "PagWeb");
     private final String name;
     private final String errorName;
 
